a9f90c3088ca3005ac923787d50dd19c0daf59dc
camilacintioli <camila.cintioli@gmail.com>
2020-02-07 11:04:10 -0300

    Add Navbar and Footer

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from '../components/Button';
+
+export default function NavBar(){
+    return(
+    <>
+    <h5>Home</h5>
+    <h5>My boards</h5>
+    <h4>TRELLO</h4>
+    <Button>My account</Button>
+    </>)
+}
